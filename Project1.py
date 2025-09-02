f7ce4d5d7f22ecc979af4d2c731d0934bcd2f88a
rommmmmashka-ops <rommmmmashka@gmail.com>
2025-09-02 21:23:10 +0300

    Add files via upload

---
+++ Project1.py
@@ -0,0 +1,14 @@
+age = 31
+username = "Andrew"
+user_balance = 42.69
+password = 'qwerty123'
+clients = ["Name1", "Name2"]
+users_passwords = {"user1": "qwerty123", "user2": "456ytrewq"}
+user_unique_data = {12, "UniqueUserName"}
+user = ("Roman", 15, "Lutsk")
+is_admin = False
+has_premium = True
+
+print(age, username, user_balance, password, clients, users_passwords, user_unique_data, user, is_admin, has_premium)
+
+print(type(age), type(username), type(user_balance), type(password), type(clients), type(users_passwords), type(user_unique_data), type(user), type(is_admin), type(has_premium))
